609df945c7b3ff0283f28d539cfe4a510c6bfa48
Matt MacManes <macmanes@gmail.com>
2015-10-05 16:40:18 -0400

    fixing up BWA stuff

--- Trinity.mk
+++ Trinity.mk
@@ -54,7 +54,7 @@ $(DIR)/$(RUN)_out_dir/$(RUN)_bwa_index.sa :$(DIR)/$(RUN)_out_dir/chrysalis/inchw
 	bwa index -p $(RUN)_bwa_index $(DIR)/$(RUN)_out_dir/chrysalis/inchworm.K25.L25.DS.fa.min100
 
 $(DIR)/$(RUN)_out_dir/chrysalis/iworm.bowtie.nameSorted.bam:$(DIR)/$(RUN)_out_dir/$(RUN)_bwa_index.sa
-	bwa mem -v 1 -p -t $CPU $(DIR)/$(RUN)_out_dir/chrysalis/inchworm.K25.L25.DS.fa.min100 $(DIR)/$(RUN)_out_dir/both.fq \
+	bwa mem -v 1 -p -t $(CPU) $(RUN)_bwa_index $(DIR)/$(RUN)_out_dir/both.fq \
 	| samtools view  -T . -bu - \
 	| samtools sort -l 0 -O bam -T tmp -@ $(CPU) -m $(grid_node_max_memory) -o $(DIR)/$(RUN)_out_dir/chrysalis/iworm.bowtie.nameSorted.bam -
 
