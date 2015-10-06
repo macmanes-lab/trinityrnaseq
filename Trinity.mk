9dabc379f97b1dcfb99c135610a32e8864ba5906
Matt MacManes <macmanes@gmail.com>
2015-10-06 06:24:23 -0400

    fixing index targets

--- Trinity.mk
+++ Trinity.mk
@@ -57,7 +57,7 @@ $(DIR)/$(RUN)_out_dir/chrysalis/$(RUN)_bwa_index.sa:$(DIR)/$(RUN)_out_dir/chrysa
 	cd $(DIR)/$(RUN)_out_dir/chrysalis/ && \
 	bwa index -p $(RUN)_bwa_index $(DIR)/$(RUN)_out_dir/chrysalis/inchworm.K25.L25.DS.fa.min100
 
-$(DIR)/$(RUN)_out_dir/chrysalis/iworm.bowtie.nameSorted.bam:$(DIR)/$(RUN)_out_dir/$(RUN)_bwa_index.sa
+$(DIR)/$(RUN)_out_dir/chrysalis/iworm.bowtie.nameSorted.bam:$(DIR)/$(RUN)_out_dir/chrysalis/$(RUN)_bwa_index.sa
 	cd $(DIR)/$(RUN)_out_dir/chrysalis/ && \
 	bwa mem -v 1 -p -t $(CPU) $(RUN)_bwa_index $(DIR)/$(RUN)_out_dir/both.fq \
 	| awk -F '\t' 'BEGIN{OFS="\t";} /^@/ {print;next;} {$$1=sprintf("%s/%d",$$1,and($$2,64)==0?2:1); print;}' \
